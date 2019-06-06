6752d76b6ef3410dbc4f2d7b95bbe2d0c65e07b0
Hwasoo Lee <Hwasoo.Lee@versanthealth.com>
2019-06-06 08:05:26 -0400

    [MASTER] Add ForEach Immutable case

---
+++ Equality/Eqaulity/Eqaulity/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Eqaulity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int three = 3;
+            int threeAgain = 3;
+
+            bool compareThem = (three == threeAgain);
+
+            // Casting value types to object boxes them
+            // Below is reference comparison
+            bool objCompare = (object)three == (object)threeAgain;
+
+
+            // Casting value types to interface boxes them
+            // Below is reference comparison
+            bool interfaceCompare = (IComparable<int>)three == (IComparable<int>)threeAgain;
+
+        }
+    }
+}
---
+++ ForEachImmutable/ForEachImmutable/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ForEachImmutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var d = new Test[3];
+            d[0] = new Test()
+            {
+                Age = 10,
+                Name = "Test1"
+            };
+            d[1] = new Test()
+            {
+                Age = 11,
+                Name = "Test2"
+            };
+            d[2] = new Test()
+            {
+                Age = 12,
+                Name = "Test3"
+            };
+
+            //When using foreach on a list of objects, is the iterated object editable?
+            // The object instance is NOT editable
+            // But the object properties are editable
+            
+            //foreach (var d1 in d)
+            //{
+            //    d1 = new Test();
+            //    Foreach iteration variable 'd1' is immutable.
+            //}
+
+            foreach (var d1 in d)
+            {
+                d1.Name = "TestAll";
+            }
+
+
+            foreach (var d1 in d)
+            {
+                Console.WriteLine($"{d1.Age} {d1.Name}");
+            }
+
+
+            for (int i = 0; i < d.Length; i++)
+            {
+                d[i] = new Test() {Age = 100, Name = "1000"};
+            }
+            
+            foreach (var d1 in d)
+            {
+                Console.WriteLine($"{d1.Age} {d1.Name}");
+            }
+
+
+        }
+    }
+
+    class Test {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
